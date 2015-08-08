24a96d005c4d1ed3aa8c562ff28f95fdac5a14f0
Andrei Aldea <andrei1998@gmail.com>
2015-08-08 19:09:48 +0300

    Fixed broken code from before

--- Birthday Timer.py
+++ Birthday Timer.py
@@ -1,43 +1,5 @@
-from Tkinter import Tk, Frame, BOTH #To Hangle Frameing
-from ttk import Frame, Button, Style #To handle Window, Theming and Buttons
-from PIL import Image, ImageTk #To handle Images, SOMETHING WRONG HERE
-
 import datetime
 time = datetime.datetime.now()
-
-class Example(Frame):
-  
-    def __init__(self, parent):
-        Frame.__init__(self, parent)   
-         
-        self.parent = parent
-        
-        self.initUI()
-        
-    def initUI(self):
-      
-        self.parent.title("Buttons")
-        self.style = Style()
-        self.style.theme_use("default")
-        
-        self.pack(fill=BOTH, expand=1)
-        
-        closeButton = Button(self, text="Close")
-        closeButton.pack(padx=5, pady=30)
-        okButton = Button(self, text="OK")
-        okButton.pack()
-
-def main():
-
-    root = Tk()
-    root.geometry("250x150+300+300")
-    app = Example(root)
-    root.mainloop()  
-
-	
-
-if __name__ == '__main__':
-    main()  
 
 print
 print
@@ -56,7 +18,7 @@ while True:
 	dt  = datetime.datetime
 	now = dt.now()
 	# This gives timedelta in days
-	delta = dt(year=2015,month=02,day=04,hour=7,minute=0,second=0) - dt(year=now.year, month=now.month, day=now.day, hour = now.hour, minute = now.minute, second = now.second)
+	delta = dt(year=2015,month=(8),day=12,hour=7,minute=0,second=0) - dt(year=now.year, month=now.month, day=now.day, hour = now.hour, minute = now.minute, second = now.second)
 	alpha = str(delta).split(":")
 	beta = str(alpha).split(" ")
 	beta = str(beta).strip("[") #Take out the [ for every reading of 33
